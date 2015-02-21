92673fc012bf8ac52521bad18a1547726222a121
sekulic <sekulic.radosav@yahoo.com>
2015-02-21 14:12:49 +0100

    add Categories

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,44 @@
+class CategoriesController < ApplicationController
+  expose(:categories)
+  expose(:category)
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+    self.category = Category.new(category_params)
+
+    if category.save
+      redirect_to category, notice: 'Category was successfully created.'
+    else
+      render action: 'new'
+    end
+  end
+
+  def update
+    if category.update(category_params)
+      redirect_to category, notice: 'Category was successfully updated.'
+    else
+      render action: 'edit'
+    end
+  end
+
+  def destroy
+    category.destroy
+    redirect_to categories_url, notice: 'Category was successfully destroyed.'
+  end
+
+  private
+    def category_params
+      params.require(:category).permit(:name)
+    end
+end
