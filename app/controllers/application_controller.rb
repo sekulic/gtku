fbb80dadcfc647c1d32f991db103e02aed81e7ec
sekulic <sekulic.radosav@yahoo.com>
2015-04-01 22:45:35 +0200

    remove redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  before_filter :redirect_to_tcs if Rails.env.production?
+  # before_filter :redirect_to_tcs if Rails.env.production?
   protect_from_forgery with: :exception
   decent_configuration do
     strategy DecentExposure::StrongParametersStrategy
